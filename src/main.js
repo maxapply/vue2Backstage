dba036e90e701201305dc0fd5860ed59a27f7063
maxapply <291325561@qq.com>
2022-10-15 17:19:00 +0800

    vue2完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,73 @@
 import Vue from 'vue'
+import {
+  Button, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb,
+  BreadcrumbItem, Dropdown, DropdownMenu, DropdownItem, Row, Col, Card, Table, TableColumn, Tag,
+  Dialog, DatePicker, Form, FormItem, Input, Select, Option, MessageBox, Message, Pagination
+
+} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+import router from '@/router'
+import store from '@/store'
+import '@/api/mock'
+import Cookie from "js-cookie";
 
 Vue.config.productionTip = false
+Vue.use(Button);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItemGroup);
+Vue.use(MenuItem);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Card);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Tag);
+Vue.use(Dialog);
+Vue.use(DatePicker);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Pagination);
+
+
+
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$message = Message;
+
+
+router.beforeEach((to, from, next) => {
+  const token = Cookie.get('token')
+  if (!token && to.name !== 'login') {
+    next({ name: 'login' })
+  } else if (token && to.name === 'login') {
+    next({ name: 'home' })
+  } else {
+    next()
+  }
+})
+
 
 new Vue({
+  router,
+  store,
+  created() {
+    store.commit('addMenu', router)
+  },
   render: h => h(App),
 }).$mount('#app')
